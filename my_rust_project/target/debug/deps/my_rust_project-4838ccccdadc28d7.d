f681bd1bdb7974630ad110935fe1121a94a120f6
svenreyes <svenxx64@gmail.com>
2025-01-26 01:22:02 +0000

    first

---
+++ my_rust_project/target/debug/deps/my_rust_project-4838ccccdadc28d7.d
@@ -0,0 +1,5 @@
+/workspaces/rust-dev/my_rust_project/target/debug/deps/my_rust_project-4838ccccdadc28d7: src/main.rs
+
+/workspaces/rust-dev/my_rust_project/target/debug/deps/my_rust_project-4838ccccdadc28d7.d: src/main.rs
+
+src/main.rs:
---
+++ my_rust_project/target/debug/my_rust_project.d
@@ -0,0 +1,1 @@
+/workspaces/rust-dev/my_rust_project/target/debug/my_rust_project: /workspaces/rust-dev/my_rust_project/src/main.rs
